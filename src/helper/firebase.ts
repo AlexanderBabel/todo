15063389515cfb0ce8ada2c7533aac130e7d0ede
Jonas Embach <j.embach@bit-production.com>
2020-06-30 00:08:42 +0200

    fix(deleteTodo): access the collection instead the firestore

--- src/helper/firebase.ts
+++ src/helper/firebase.ts
@@ -4,7 +4,6 @@ import { FirebaseTodo } from '../types/todo';
 admin.initializeApp();
 const { fromDate } = admin.firestore.Timestamp;
 export const collection = admin.firestore().collection('todos');
-export const firestore = admin.firestore();
 
 export async function addTodo(name: string, dueDate?: Date): Promise<FirebaseTodo> {
   const data = {
@@ -22,7 +21,7 @@ export async function addTodo(name: string, dueDate?: Date): Promise<FirebaseTod
 }
 
 export async function deleteTodo(id: string): Promise<boolean> {
-  const doc = firestore.doc(`todos/${id}`);
+  const doc = collection.doc(id);
 
   return await doc.delete().then(() => true);
 }
